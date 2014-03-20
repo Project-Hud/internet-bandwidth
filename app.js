ca6f8d28d8c6473756e1a6534ddec51da2d1ed47
James Mortemore <james.mortemore@clock.co.uk>
2014-03-20 10:25:14 +0000

    Updated to use hud-widget

--- app.js
+++ app.js
@@ -1,46 +1,18 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , http = require('http')
-  , path = require('path')
+var Widget = new require('hud-widget')
+  , widget = new Widget()
   , getData = require('./lib/get-data')
   , processData = require('./lib/process-data')
-  , app = express()
-
-// all environments
-app.set('port', process.env.PORT || 3000)
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-app.use(express.favicon())
-app.use(express.logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.methodOverride())
-app.use(app.router)
-app.use(express.static(path.join(__dirname, 'public')))
-
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler())
-}
 
 var url = process.env.DATA_URL
 
-app.get('/', function (req, res) {
+widget.get('/', function (req, res) {
   res.render('index')
 })
 
-app.get('/get-bandwidth', function (req, res) {
+widget.get('/get-bandwidth', function (req, res) {
   getData(url, function (error, data) {
     if (error) console.warn(error)
 
     res.json(processData(data))
   })
-})
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'))
 })
